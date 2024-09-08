ff4dec5e04b8345231beab09fb25d075e4ff5632
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2024-09-07 22:31:32 -0400

    first

---
+++ resources/js/Layouts/AppMenu.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'Home',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+    },
+    {
+        label: 'Menu 1',
+        items: [
+            { label: 'Example 1', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+            { label: 'Example 2', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+            { label: 'Example 3', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+        ]
+    },
+    {
+        label: 'Hierarchy',
+        items: [
+            {
+                label: 'Submenu 1',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 1.1',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+                        ]
+                    },
+                    {
+                        label: 'Submenu 1.2',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                    }
+                ]
+            },
+            {
+                label: 'Submenu 2',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 2.1',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                        ]
+                    },
+                    {
+                        label: 'Submenu 2.2',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                    }
+                ]
+            }
+        ]
+    }
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+</script>
+
+<template>
+    <AppLayout>
+        <div class="flex items-center justify-center w-full h-full p-3">
+            <div>Sua proxima grande ideia...</div>
+        </div>
+    </AppLayout>
+</template>
